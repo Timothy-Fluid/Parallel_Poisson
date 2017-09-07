37d9ff665d6bcd958131111d1be2362850ada993
Timothy-Fluid <timothy@buaa.edu.cn>
2017-09-07 12:14:27 -0400

    first commit

---
+++ Parallel_Jacobi.py
@@ -0,0 +1,94 @@
+import numpy as np
+import mpi4py.MPI as MPI
+import sys
+import time
+
+
+def smoother_Jacobi(phi, phi_0, f, h):
+    h2 = h**2
+    phi[1:-1, 1:-1] = 0.25 * (phi_0[:-2, 1:-1] + phi_0[1:-1, :-2] +
+                              phi_0[2:, 1:-1] + phi_0[1:-1, 2:] + h2 * f[1:-1, 1:-1])
+
+
+# Initialize the parallel environment
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+n_nodes = int(sys.argv[1])
+ntmax = int(sys.argv[2])
+
+local_size = int((n_nodes - 1) / size + 1)
+local_length = 1. / size
+h = 1. / (n_nodes - 1)
+
+x = np.linspace(0, 1, n_nodes, dtype=np.float64)
+y = np.linspace(rank * local_length, (rank + 1.) *
+                local_length, local_size, dtype=np.float64)
+X, Y = np.meshgrid(x, y)
+local_phi = np.zeros((local_size + 2, n_nodes), dtype=np.float64)
+# left and right boundary
+local_phi[1:-1, 0] = np.exp(X[:, 0]) * np.exp(-2. * Y[:, 0])
+local_phi[1:-1, -1] = np.exp(X[:, -1]) * np.exp(-2 * Y[:, -1])
+
+# Force the boundary to Procs 0 and size-1
+if rank == 0:
+    local_phi[1, :] = np.exp(X[0, :]) * np.exp(-2. * Y[0, :])
+if rank == size - 1:
+    local_phi[-2, :] = np.exp(X[-1, :]) * np.exp(-2. * Y[-1, :])
+
+# Fource source setting
+local_f = np.zeros((local_size + 2, n_nodes), dtype=np.float64)
+local_f[1:-1, :] = -5. * np.exp(X) * np.exp(-2. * Y)
+
+res = np.empty(1, dtype=np.float64)
+# Decide the domain
+begin_row = 0
+end_row = local_size + 2
+if rank == 0:
+    begin_row = 1
+if rank == size - 1:
+    end_row = local_size + 1
+
+#print("After initialization" + "Procs " + str(rank))
+# print(local_phi)
+begin_time = time.time()
+for nt in range(ntmax):
+    # Receive data from blocks below
+    if rank < (size - 1):
+        comm.Recv([local_phi[-1, :], MPI.DOUBLE], source=rank + 1, tag=1)
+    # Send data to blocks above
+    if rank > 0:
+        comm.Send([local_phi[2, :], MPI.DOUBLE], dest=rank - 1, tag=1)
+    # Send data to blocks below
+    if rank < (size - 1):
+        comm.Send([local_phi[-3, :], MPI.DOUBLE], dest=rank + 1, tag=2)
+    # Receive data from blocks above
+    if rank > 0:
+        comm.Recv([local_phi[0, :], MPI.DOUBLE], source=rank - 1, tag=2)
+#    print("Iteration " + str(nt) + " Procs " +
+#          str(rank) + "After communication")
+#    print(local_phi)
+    local_phi0 = local_phi.copy()
+    smoother_Jacobi(local_phi[begin_row:end_row, :],
+                    local_phi0[begin_row:end_row, :], local_f[begin_row:end_row, :], h)
+    if rank == size - 1:
+        local_res = np.array(
+            [np.sum((local_phi[1:-1, :] - local_phi0[1:-1, :])**2)])
+    else:
+        local_res = np.array(
+            [np.sum((local_phi[1:-2, :] - local_phi0[1:-2, :])**2)])
+
+    comm.Allreduce(local_res, res, op=MPI.SUM)
+    res = np.sqrt(res)
+    if rank == 0:
+        print("Iteration " + str(nt) + " Res " + str(res[0]))
+
+    if res[0] <= 1.e-5:
+        print("The solution has converged")
+        break
+
+end_time = time.time()
+if rank == 0:
+    print("Time taken: " + str(end_time - begin_time))
+    # Residual Plot
---
+++ Serial.py
@@ -0,0 +1,92 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import time
+
+# smoother
+# now jacobi G-S SOR are available
+
+
+def smoother_Jacobi(phi, phi_0, f, h):
+    h2 = h**2
+    phi[1:-1, 1:-1] = 0.25 * (phi_0[:-2, 1:-1] + phi_0[1:-1,
+                                                       :-2] + phi_0[2:, 1:-1]
+                              + phi_0[1:-1, 2:] + h2 * f[1:-1, 1:-1])
+
+
+def smoother_GS(phi, f, n_nodes):
+    h2 = (1. / (n_nodes - 1.))**2
+    for i in range(1, n_nodes - 1):
+        for j in range(1, n_nodes - 1):
+            phi[i, j] = 0.25 * \
+                (phi[i - 1, j] + phi[i, j - 1] +
+                 phi[i + 1, j] + phi[i, j + 1] + h2 * f[i, j])
+
+
+def smoother_SOR(phi, f, n_nodes):
+    omega = 1.5
+    h2 = (1. / (n_nodes - 1.))**2
+    for i in range(1, n_nodes - 1):
+        for j in range(1, n_nodes - 1):
+            phi[i, j] = (1. - omega) * phi[i, j]\
+                + omega * 0.25 * \
+                (phi[i - 1, j] + phi[i, j - 1] +
+                 phi[i + 1, j] + phi[i, j + 1] + h2 * f[i, j])
+
+
+n_nodes = int(sys.argv[1])
+type_smoother = int(sys.argv[2])
+ntmax = int(sys.argv[3])
+
+h = 1. / (n_nodes - 1)
+# Initialize the array
+x = np.linspace(0, 1, n_nodes, dtype=np.float64)
+y = np.linspace(0, 1, n_nodes, dtype=np.float64)
+X, Y = np.meshgrid(x, y)
+
+
+# Force the boundary conditions
+phi = np.exp(X) * np.exp(-2. * Y)
+phi[1:-1, 1:-1] = 0.0
+
+
+# Calculate the force source
+f = -5. * np.exp(X) * np.exp(-2. * Y)
+begin_time = time.time()
+if type_smoother == 1:
+    for i in range(ntmax):
+        phi_0 = phi.copy()
+        smoother_Jacobi(phi, phi_0, f, h)
+        res = np.linalg.norm(phi - phi_0)
+        print("Iteration " + str(i) + "Res = " + str(res))
+        if res <= 1.e-5:
+            print("Converge")
+            break
+elif type_smoother == 2:
+    for i in range(10000):
+        phi_0 = phi.copy()
+        smoother_GS(phi, f, n_nodes)
+        res = np.linalg.norm(phi - phi_0)
+        print("Iteration " + str(i) + "Res = " + str(res))
+        if res <= 1.e-5:
+            print("Converge")
+            break
+elif type_smoother == 3:
+    for i in range(10000):
+        phi_0 = phi.copy()
+        smoother_SOR(phi, f, n_nodes)
+        res = np.linalg.norm(phi - phi_0)
+        print("Iteration " + str(i) + "Res = " + str(res))
+        if res <= 1.e-5:
+            print("Converge")
+            break
+
+end_time = time.time()
+print("Time taken: " + str(end_time - begin_time))
+np.savetxt("Serial.txt", phi)
+# Plot
+plt.figure
+CS = plt.contour(X, Y, phi)
+plt.clabel(CS, inline=1, fontsize=10)
+plt.title('Pressure')
+plt.show()
